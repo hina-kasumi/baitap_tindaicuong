108590146b8442fe0242e2bf98515775c1107a6c
HinaKasumi <nguyenhaihieu89@gmail.com>
2023-12-01 17:19:32 +0700

    fix bug bai 2-5

--- part2/bai2-5.cpp
+++ part2/bai2-5.cpp
@@ -1,17 +1,33 @@
 /*Viết chương trình nhập vào một số nguyên n gồm ba chữ số. xuất ra màn hình chữ số lớn nhất ở vị trí nào?*/
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    char n[4];
-    gets(n);
+    char n[5];
+    back: gets(n);
+    if(n[0]=='0' || n[1]=='0') goto back;
     int maxx = -1, vitri = 0;
-    for(int i = 0; i <= 3; i++){
-        if(n[i]> maxx){
-            maxx = n[i];
-            vitri = i+1;
+    if (n[0] != '-')
+    {
+        for (int i = 0; i <= 3; i++)
+        {
+            if (n[i] > maxx)
+            {
+                maxx = n[i];
+                vitri = i + 1;
+            }
         }
     }
-        printf("%c %d", maxx, vitri);
-    
+    else
+    {
+        for (int i = 2; i < 4; i++)
+        {
+            if (n[i] > maxx)
+            {
+                maxx = n[i];
+                vitri = i;
+            }
+        }
+    }
+    printf("%c %d", maxx, vitri);
     return 0;
 }
---
+++ part2/bai2-6.cpp
@@ -0,0 +1,8 @@
+/*. Viết chương trình nhập vào số nguyên n gồm ba chữ số. Xuất ra màn hình theo thứ tự tăng dần của các 
+chữ số.*/
+#include<stdio.h>
+int main()
+{
+    
+    return 0;
+}
