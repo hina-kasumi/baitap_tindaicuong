7a8ec7e8d5df0cd21026713a681024c510733fb1
HinaKasumi <nguyenhaihieu89@gmail.com>
2024-06-27 18:53:09 +0700

    test

---
+++ KyThuatLapTtrinh/DeKiemTraGK/bai1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define file "FileName"
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define FIN(a) freopen(a".INP", "r", stdin);
+#define FOUT(a) freopen(a".OUT", "w", stdout);
+
+
+
+int main()
+{
+    
+
+    return 0;
+}
