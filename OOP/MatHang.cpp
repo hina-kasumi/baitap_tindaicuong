0626f28d80e59c0862518c80e0a548e1e5862560
Nguyễn Hải Hiếu <nguyenhaihieu89@gmail.com>
2024-09-17 15:20:58 +0700

    OOP

--- OOP/MatHang.cpp
+++ OOP/MatHang.cpp
@@ -37,37 +37,72 @@ public:
     friend ostream &operator<<(ostream &os, MatHang x)
     {
         os << x.maHang << '|'
-            << x.tenHang << '|'
-            << x.nhaSanXuat << '|'
-            << x.soLuong << '|'
-            << x.donGia << "|";
+           << x.tenHang << '|'
+           << x.nhaSanXuat << '|'
+           << x.soLuong << '|'
+           << x.donGia << "|";
         return os;
     }
 };
 
-class MayTinh:public MatHang{
+class MayTinh : public MatHang
+{
 private:
     string loaiCPU, heDieuHanh;
     float trongLuong;
+
 public:
-    MayTinh(int maHang = 0, string tenHang = "", string nhaSanXuat = "", int soLuong = 0, float donGia = 0, string loaiCPU = "",string heDieuHanh = "window", float trongLuong = 0){
+    MayTinh(int maHang = 0, string tenHang = "", string nhaSanXuat = "", int soLuong = 0, float donGia = 0, string loaiCPU = "", string heDieuHanh = "window", float trongLuong = 0)
+    {
         MatHang(maHang, tenHang, nhaSanXuat, soLuong, donGia);
         this->loaiCPU = loaiCPU;
         this->heDieuHanh = heDieuHanh;
         this->trongLuong = trongLuong;
     }
 
-    friend ostream &operator<<(ostream& os, MayTinh x){
+    friend ostream &operator<<(ostream &os, MayTinh x)
+    {
         os << (MatHang)x << x.loaiCPU << '|' << x.heDieuHanh << '|' << x.trongLuong << '\n';
         return os;
     }
+
+    bool equal(string tim)
+    {
+        return tim == tenHang;
+    }
+
+    MayTinh max(MayTinh x)
+    {
+        return this->donGia > x.donGia ? *this : x;
+    }
 };
-
-
 
 signed main()
 {
-    
+    int n;
+    cin >> n;
+    MayTinh maytinh[n];
+    for (MayTinh &x : maytinh)
+        cin >> x;
+
+    cout << "tim thong thong tin mat hang" << endl;
+    string tenhang;
+    cin >> tenhang;
+
+    for (MayTinh x : maytinh)
+    {
+        if (x.equal(tenhang))
+        {
+            cout << x << endl;
+        }
+    }
+
+    MayTinh res;
+    for(MayTinh x: maytinh){
+        res = res.max(x);
+    }
+    cout << "gia tri lon nhat: ";
+    cout << res << endl;
 
     return 0;
 }
